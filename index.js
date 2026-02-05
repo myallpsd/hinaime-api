4d9bc1ad3cb5b85bc1a07e9e17a755082280d056
myallpsd <myallpsd@gmail.com>
2026-02-06 02:15:04 +0600

    in cres the time of casg to 10h

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ Bun.serve({
 console.log(`server is running at http://0.0.0.0:${PORT}`);
 console.log(`docs: http://0.0.0.0:${PORT}/docs`);
 console.log(`swagger: http://0.0.0.0:${PORT}`);
-const CACHE_CLEAR_INTERVAL = 30 * 60 * 1000;
+const CACHE_CLEAR_INTERVAL = 600 * 60 * 1000;
 
 setInterval(async () => {
   console.log('Running scheduled cache clear...');
