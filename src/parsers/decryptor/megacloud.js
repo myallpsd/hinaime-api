5165ef348c28babf774763bb50ae70c7ffeb7bdb
myallpsd <myallpsd@gmail.com>
2026-02-26 11:52:05 +0600

    update keys

--- src/parsers/decryptor/megacloud.js
+++ src/parsers/decryptor/megacloud.js
@@ -20,14 +20,7 @@ async function getDecryptionKey() {
   }
 
   try {
-    const { data: key } = await axios.get(
-      'https://raw.githubusercontent.com/ryanwtf88/megacloud-keys/refs/heads/master/key.txt',
-      { timeout: 5000 }
-    );
-
-    cachedKey = key.trim();
-    keyLastFetched = now;
-    console.log('Decryption key fetched and cached');
+    cachedKey = "3709ad8892f413166b796a10c7fb86018bd1be1c7ae6f4d2cfc3fdc299cb3205";
     return cachedKey;
   } catch (error) {
     console.error('Failed to fetch decryption key:', error.message);
@@ -36,7 +29,6 @@ async function getDecryptionKey() {
       console.log('Using expired cached key as fallback');
       return cachedKey;
     }
-    throw new Error('Unable to fetch decryption key');
   }
 }
 
